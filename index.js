06af959dbe5aa687a0dea55b55b14c9eb67689ea
Andrew Meek <andrewmeek@MacBook-Air-AndMeek.local>
2024-08-10 03:46:12 +0300

    start project

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const chalk = require('chalk');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const {
   addNote,
@@ -7,6 +11,8 @@ const {
   removeNote,
   updateNote,
 } = require('./notes.controller');
+const { addUser, loginUser } = require('./user.controller');
+const auth = require('./middlewares/auth');
 
 const PORT = 3001;
 
@@ -17,6 +23,7 @@ app.set('views', 'pages');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(
   express.urlencoded({
@@ -24,39 +31,135 @@ app.use(
   })
 );
 
+app.get('/login', async (req, res) => {
+  res.render('login', {
+    title: 'Express App',
+    error: undefined,
+  });
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const token = await loginUser(req.body.email, req.body.password);
+
+    res.cookie('token', token, { httpOnli: true });
+
+    res.redirect('/');
+  } catch (e) {
+    res.render('register', {
+      title: 'Express App',
+      error: e.message,
+    });
+  }
+});
+
+app.get('/register', async (req, res) => {
+  res.render('register', {
+    title: 'Express App',
+    error: undefined,
+  });
+});
+
+app.post('/register', async (req, res) => {
+  try {
+    await addUser(req.body.email, req.body.password);
+    res.redirect('/login');
+  } catch (e) {
+    if (e.code === 11000) {
+      res.render('register', {
+        title: 'Express App',
+        error: 'Email is already registred',
+      });
+      return;
+    }
+    console.log(e);
+    res.render('register', {
+      title: 'Express App',
+      error: e.message,
+    });
+  }
+});
+
+app.get('/logout', (req, res) => {
+  res.cookie('token', '', { httpOnly: true });
+
+  res.redirect('./login');
+});
+
+app.use(auth);
+
 app.get('/', async (req, res) => {
   res.render('index', {
     title: 'Express App',
     notes: await getNotes(),
+    userEmail: req.user.email,
     created: false,
+    error: false,
   });
 });
 
 app.post('/', async (req, res) => {
-  await addNote(req.body.title);
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: true,
-  });
+  try {
+    await addNote(req.body.title, req.user.email);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: true,
+      error: false,
+    });
+  } catch (e) {
+    console.error('Creation error', e);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: true,
+    });
+  }
 });
 
 app.delete('/:id', async (req, res) => {
-  await removeNote(req.params.id);
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: false,
-  });
+  try {
+    await removeNote(req.params.id, req.user.email);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: false,
+    });
+  } catch (e) {
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: e.message,
+    });
+  }
 });
 
 app.put('/:id', async (req, res) => {
-  await updateNote(req.params.id, req.body.title);
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: false,
-  });
+  try {
+    await updateNote(req.params.id, req.body.title, req.user.email);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: false,
+    });
+  } catch (e) {
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: e.message,
+    });
+  }
 });
 
 // const server = http.createServer(async (req, res) => {
@@ -84,7 +187,10 @@ app.put('/:id', async (req, res) => {
 //     });
 //   }
 // });
+const URL = process.env.MONGOOSE_CONNECT_STRING;
 
-app.listen(PORT, () => {
-  console.log(chalk.green(`server has been started on port ${PORT}...`));
+mongoose.connect(URL).then(() => {
+  app.listen(PORT, () => {
+    console.log(chalk.green(`server has been started on port ${PORT}...`));
+  });
 });
--- notes.controller.js
+++ notes.controller.js
@@ -1,61 +1,32 @@
-const fs = require('fs/promises');
-const path = require('path');
 const chalk = require('chalk');
+const Note = require('./models/note');
 
-const notePath = path.join(__dirname, 'db.json');
+async function addNote(title, owner) {
+  await Note.create({ title, owner });
 
-async function addNote(title) {
-  const notes = await getNotes();
-
-  const note = {
-    title,
-    id: Date.now().toString(),
-  };
-
-  notes.push(note);
-
-  await fs.writeFile(notePath, JSON.stringify(notes));
   console.log(chalk.green('Note was added'));
 }
 
 async function getNotes() {
-  const notes = await fs.readFile(notePath, { encoding: 'utf-8' });
-  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+  const notes = await Note.find();
+  return notes;
 }
 
-async function printNotes() {
-  const notes = await getNotes();
-
-  console.log(chalk.bgBlue('Here is the list of notes:'));
-  notes.forEach((note) =>
-    console.log(chalk.blue(note.id), chalk.blue(note.title))
-  );
-}
-
-async function removeNote(id) {
-  const notes = await getNotes();
-  const updatedNotes = notes.filter(
-    ({ id: notesId }) => notesId !== String(id)
-  );
-
-  await fs.writeFile(notePath, JSON.stringify(updatedNotes));
+async function removeNote(id, owner) {
+  const result = await Note.deleteOne({ _id: id, owner });
+  if (result.matchedCount === 0) {
+    throw new Error('No note to delete');
+  }
 
   console.log(chalk.red('Note was removed'));
 }
 
-async function updateNote(id, title) {
-  const notes = await getNotes();
-
-  const updatedNotes = notes.map((note) => {
-    if (note.id === id) {
-      return {
-        title,
-        id,
-      };
-    }
-    return note;
-  });
-  await fs.writeFile(notePath, JSON.stringify(updatedNotes));
+async function updateNote(id, title, owner) {
+  const result = await Note.updateOne({ _id: id, title }, owner);
+  if (result.matchedCount === 0) {
+    throw new Error('No note to edit');
+  }
+  console.log(chalk.green(`Note with id=${id} was updated`));
 }
 
 module.exports = {
